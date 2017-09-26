8dbba40a097f5c406a36011d613c6d40e41714e4
Tobias Brunner <tobias@strongswan.org>
2017-09-26 14:57:40 +0200

    unit-tests: Warn if we skip RSA tests due to dependencies

--- src/libstrongswan/tests/suites/test_rsa.c
+++ src/libstrongswan/tests/suites/test_rsa.c
@@ -57,6 +57,8 @@ static void test_good_sig(private_key_t *privkey, public_key_t *pubkey)
 			!lib->plugins->has_feature(lib->plugins,
 						PLUGIN_PROVIDE(PRIVKEY_SIGN, schemes[i])))
 		{
+			warn("%N not supported, skip scheme", signature_scheme_names,
+				 schemes[i]);
 			continue;
 		}
 		if (schemes[i] == SIGN_RSA_EMSA_PKCS1_PSS)
@@ -119,6 +121,8 @@ static void test_bad_sigs(public_key_t *pubkey)
 		if (!lib->plugins->has_feature(lib->plugins,
 						PLUGIN_PROVIDE(PUBKEY_VERIFY, schemes[s])))
 		{
+			warn("%N not supported, skip scheme", signature_scheme_names,
+				 schemes[s]);
 			continue;
 		}
 		if (schemes[s] == SIGN_RSA_EMSA_PKCS1_PSS)
@@ -1408,6 +1412,7 @@ START_TEST(test_sign_pkcs15_2048)
 		!lib->plugins->has_feature(lib->plugins,
 					PLUGIN_PROVIDE(PRIVKEY_SIGN, scheme)))
 	{
+		warn("%N not supported, skip test", signature_scheme_names, scheme);
 		return;
 	}
 	fail_unless(privkey_2048->sign(privkey_2048, scheme, NULL, pkcs15_2048[_i].m, &sig),
@@ -2548,6 +2553,7 @@ START_TEST(test_sign_pkcs15_3072)
 		!lib->plugins->has_feature(lib->plugins,
 					PLUGIN_PROVIDE(PRIVKEY_SIGN, scheme)))
 	{
+		warn("%N not supported, skip test", signature_scheme_names, scheme);
 		return;
 	}
 	fail_unless(privkey_3072->sign(privkey_3072, scheme, NULL, pkcs15_3072[_i].m, &sig),
@@ -3200,6 +3206,8 @@ START_TEST(test_sign_pss_no_salt)
 		!lib->plugins->has_feature(lib->plugins,
 					PLUGIN_PROVIDE(PRIVKEY_SIGN, SIGN_RSA_EMSA_PKCS1_PSS)))
 	{
+		warn("%N not supported, skip test", signature_scheme_names,
+			 SIGN_RSA_EMSA_PKCS1_PSS);
 		return;
 	}
 	privkey = lib->creds->create(lib->creds, CRED_PRIVATE_KEY, KEY_RSA,
@@ -3449,6 +3457,7 @@ START_TEST(test_verify_pkcs15)
 	if (!lib->plugins->has_feature(lib->plugins,
 					PLUGIN_PROVIDE(PUBKEY_VERIFY, scheme)))
 	{
+		warn("%N not supported, skip test", signature_scheme_names, scheme);
 		return;
 	}
 	for (i = 0; i < countof(pkcs15_verify[_i].tests); i++)
@@ -4411,6 +4420,8 @@ START_TEST(test_verify_pss)
 	if (!lib->plugins->has_feature(lib->plugins,
 					PLUGIN_PROVIDE(PUBKEY_VERIFY, SIGN_RSA_EMSA_PKCS1_PSS)))
 	{
+		warn("%N not supported, skip test", signature_scheme_names,
+			 SIGN_RSA_EMSA_PKCS1_PSS);
 		return;
 	}
 	for (i = 0; i < countof(pss_verify[_i].tests); i++)
